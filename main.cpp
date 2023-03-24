5855fb87fe3194cbc825567c7ea46436b80be9f6
Th3Spl <86083815+Th3Spl@users.noreply.github.com>
2023-03-24 13:56:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,8 @@
 //Date: 12/9/2022
 //Discord: https://bit.ly/3OkuXMT
 /*Purposes: 
-* This (really)simple c++ code will give you all the saved wifi profile
+* This (really)simple c++ code will give you all the saved wifi profiles
 * Note: USE THIS ONLY ON YOUR COMPUTER!
-* DONE'T USE THIS FOR MAKE ANY TYPE OF DAMAGE TO SOMEONE ELSE
 * THIS IS FOR EDUCATIONAL PURPOSES ONLY
 */
 
